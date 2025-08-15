65d36c609bbcaff91d39064c484eea83421e7a5c
Nikolay Dachev <nikolay.dachev@suse.com>
2025-08-15 16:17:54 +0300

    J vrouter (#2)
    * init vjr
    
    * update vjr lab interfaces
    
    * update run all sh sciprt
    
    * add vjr dump config file
    
    * fix isis missing lo interfaces

--- junos_mpls_config.sh
+++ junos_mpls_config.sh
@@ -4,6 +4,8 @@
 LABEL_PROTOCOL="rsvp"
 IGP_PROTOCOL="ospf"
 ECMP_ENABLED="false"
+HW_GROUP="vjr"
+LS_GROUP="vjr_logical_systems"
 
 # Function to display usage
 usage() {
@@ -12,12 +14,15 @@ usage() {
     echo "  -l, --label-protocol {ldp|rsvp}    Label distribution protocol (default: rsvp)"
     echo "  -i, --igp-protocol {ospf|isis}     Interior gateway protocol (default: ospf)"
     echo "  -e, --ecmp {enable|disable}        Enable/disable ECMP (default: disable)"
+    echo "  -g, --hw-group GROUP               Hardware group name (default: vjr)"
+    echo "  -s, --ls-group GROUP               Logical systems group name (default: vjr_logical_systems)"
     echo "  -h, --help                         Display this help message"
     echo ""
     echo "Examples:"
-    echo "  $0                                 # Use all defaults (rsvp, ospf, ecmp disabled)"
+    echo "  $0                                 # Use all defaults"
     echo "  $0 -l ldp -i isis -e enable        # Use LDP, ISIS, and enable ECMP"
     echo "  $0 --label-protocol ldp --ecmp enable  # Use LDP with ECMP enabled"
+    echo "  $0 -g juniper -s acx1100_logical_systems  # Use custom group names"
     exit 1
 }
 
@@ -55,6 +60,24 @@ while [[ $# -gt 0 ]]; do
                 exit 1
             fi
             ;;
+        -g|--hw-group)
+            if [[ -n "$2" && "$2" != -* ]]; then
+                HW_GROUP="$2"
+                shift 2
+            else
+                echo "Error: Hardware group name required after $1"
+                exit 1
+            fi
+            ;;
+        -s|--ls-group)
+            if [[ -n "$2" && "$2" != -* ]]; then
+                LS_GROUP="$2"
+                shift 2
+            else
+                echo "Error: Logical systems group name required after $1"
+                exit 1
+            fi
+            ;;
         -h|--help)
             usage
             ;;
@@ -70,32 +93,34 @@ printf "\n=== Configuration Summary ===\n"
 printf "Label Protocol: %s\n" "$LABEL_PROTOCOL"
 printf "IGP Protocol: %s\n" "$IGP_PROTOCOL"
 printf "ECMP: %s\n" "$([ "$ECMP_ENABLED" == "true" ] && echo "enabled" || echo "disabled")"
+printf "Hardware Group: %s\n" "$HW_GROUP"
+printf "Logical Systems Group: %s\n" "$LS_GROUP"
 printf "==============================\n\n"
 
 # Initialize step counter
 STEP=1
 
 # Start configuration deployment
-printf "\n%02d - Configure physical interfaces\n\n" $STEP
-ansible-playbook playbooks/juniper/interfaces.yml -e 'group=juniper'
+printf "\n%02d - Configure logical systems\n\n" $STEP
+ansible-playbook playbooks/juniper/logical-systems.yml -e "group=$HW_GROUP"
 ((STEP++))
 
-printf "\n%02d - Configure logical systems\n\n" $STEP
-ansible-playbook playbooks/juniper/logical-systems.yml
+printf "\n%02d - Configure physical interfaces\n\n" $STEP
+ansible-playbook playbooks/juniper/interfaces.yml -e "group=$HW_GROUP"
 ((STEP++))
 
 printf "\n%02d - Configure LS's interfaces\n\n" $STEP
-ansible-playbook playbooks/juniper/interfaces.yml -e 'group=acx1100_logical_systems' --forks 1
+ansible-playbook playbooks/juniper/interfaces.yml -e "group=$LS_GROUP" --forks 1
 ((STEP++))
 
 printf "\n%02d - Configure LS's common configuration\n\n" $STEP
-ansible-playbook playbooks/juniper/common.yml -e 'group=acx1100_logical_systems' --forks 1
+ansible-playbook playbooks/juniper/common.yml -e "group=$LS_GROUP" --forks 1
 ((STEP++))
 
 # Conditionally configure ECMP
 if [[ "$ECMP_ENABLED" == "true" ]]; then
     printf "\n%02d - Configure LS's ECMP\n\n" $STEP
-    ansible-playbook playbooks/juniper/enable_ecmp.yml -e 'group=acx1100_logical_systems' --forks 1
+    ansible-playbook playbooks/juniper/enable_ecmp.yml -e "group=$LS_GROUP" --forks 1
     ((STEP++))
 else
     printf "\n-- - Skipping ECMP configuration (disabled)\n\n"
@@ -104,32 +129,32 @@ fi
 # Configure selected IGP protocol
 if [[ "$IGP_PROTOCOL" == "isis" ]]; then
     printf "\n%02d - Configure LS's ISIS\n\n" $STEP
-    ansible-playbook playbooks/juniper/isis.yml -e 'group=acx1100_logical_systems' --forks 1
+    ansible-playbook playbooks/juniper/isis.yml -e "group=$LS_GROUP" --forks 1
 else
     printf "\n%02d - Configure LS's OSPF\n\n" $STEP
-    ansible-playbook playbooks/juniper/ospf.yml -e 'group=acx1100_logical_systems' --forks 1
+    ansible-playbook playbooks/juniper/ospf.yml -e "group=$LS_GROUP" --forks 1
 fi
 ((STEP++))
 
 printf "\n%02d - Configure LS's MPLS\n\n" $STEP
-ansible-playbook playbooks/juniper/mpls.yml -e 'group=acx1100_logical_systems' --forks 1
+ansible-playbook playbooks/juniper/mpls.yml -e "group=$LS_GROUP" --forks 1
 ((STEP++))
 
 # Configure selected label distribution protocol
 if [[ "$LABEL_PROTOCOL" == "ldp" ]]; then
     printf "\n%02d - Configure LS's LDP\n\n" $STEP
-    ansible-playbook playbooks/juniper/ldp.yml -e 'group=acx1100_logical_systems' --forks 1
+    ansible-playbook playbooks/juniper/ldp.yml -e "group=$LS_GROUP" --forks 1
 else
     printf "\n%02d - Configure LS's RSVP\n\n" $STEP
-    ansible-playbook playbooks/juniper/rsvp.yml -e 'group=acx1100_logical_systems' --forks 1
+    ansible-playbook playbooks/juniper/rsvp.yml -e "group=$LS_GROUP" --forks 1
 fi
 ((STEP++))
 
 printf "\n%02d - Configure LS's iBGP\n\n" $STEP
-ansible-playbook playbooks/juniper/ibgp.yml -e 'group=acx1100_logical_systems' --forks 1
+ansible-playbook playbooks/juniper/ibgp.yml -e "group=$LS_GROUP" --forks 1
 ((STEP++))
 
 printf "\n%02d - Configure LS's L3VPN\n\n" $STEP
-ansible-playbook playbooks/juniper/l3vpn.yml -e 'group=acx1100_logical_systems' --forks 1
+ansible-playbook playbooks/juniper/l3vpn.yml -e "group=$LS_GROUP" --forks 1
 
 printf "\nDONE!\n\n"
